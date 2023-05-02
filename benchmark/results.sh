310f12bced222c57c9bae5bd9cb04cdc7211009d
George Vasiliu <george.vasiliu@rubylife.com>
2023-05-01 21:16:45 -0400

    update

--- benchmark/results.sh
+++ benchmark/results.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -f results.txt; for i in `ls -1 *py | egrep -v BENCH`;do ( test -x $i && python3 $i | egrep Profit >> results.txt; echo >> results.txt);done
+rm -f results.txt; date > results.txt; for i in `ls -1 *py | egrep -v BENCH`;do ( test -x $i && python3 $i | egrep Profit >> results.txt; echo >> results.txt);done
 
