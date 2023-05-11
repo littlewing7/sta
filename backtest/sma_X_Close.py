fe7632168225b73f76d04d338391c0cd84aef260
George Vasiliu <george.vasiliu@rubylife.com>
2023-05-11 17:04:07 -0400

    backtest results update

---
+++ backtest/sma_X_Close.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import yfinance as yf
+import pandas as pd
+import argparse
+
+
+def __SMA ( data, n ):
+    data['SMA_{}'.format(n)] = data['Close'].rolling(window=n).mean()
+    return data
+
+def __BB (data, window=20):
+    std = data['Close'].rolling(window).std()
+    data = __SMA ( data, window )
+    data['BB_upper']   = data["SMA_20"] + std * 2
+    data['BB_lower']   = data["SMA_20"] - std * 2
+    data['BB_middle']  = data["SMA_20"]
+
+    return data
+
+
+def backtest_strategy(stock, start_date):
+    """
+    Function to backtest a strategy
+    """
+    # Download data
+    data = yf.download(stock, start=start_date, progress=False)
+
+    for ma in [ 8, 9 , 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,20, 21, 22, 23, 24, 25, 26, 27, 28,29, 30, 31, 32, 33, 34 ]:
+        data = __SMA (data, ma)
+
+        # Set initial conditions
+        position = 0
+        buy_price = 0
+        sell_price = 0
+        returns = []
+
+        # Loop through data
+        for i in range(len(data)):
+            # Buy signal
+            if data["Close"][i] > data["SMA_{}".format(ma)][i] and data["Close"][i - 1] < data["SMA_{}".format(ma)][i - 1] and position == 0:
+                position = 1
+                buy_price = data["Adj Close"][i]
+                today = data.index[i]
+                #print(f"Buying {stock} at {buy_price} @ {today}")
+
+            # Sell signal
+            elif data["Close"][i] < data["SMA_{}".format(ma)][i] and data["Close"][i - 1]  > data["SMA_{}".format(ma)][i - 1] and position == 1:
+                position = 0
+                sell_price = data["Adj Close"][i]
+                today = data.index[i]
+                #print(f"Selling {stock} at {sell_price} @ {today}")
+
+                # Calculate returns
+                returns.append((sell_price - buy_price) / buy_price)
+
+        # Calculate total returns
+        total_returns = (1 + sum(returns)) * 100000
+
+        import sys
+        name = sys.argv[0]
+
+        # Print results
+        print(f"\n{name} ::: {stock} {ma} Backtest Results ({start_date} - today)")
+        print(f"---------------------------------------------")
+        print(f"{name} ::: {stock} {ma} - Total Returns: ${total_returns:,.2f}")
+        print(f"{name} ::: {stock} {ma} - Profit/Loss: {((total_returns - 100000) / 100000) * 100:.2f}%")
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ticker', nargs='+',  type=str, help='your name')
+
+    args = parser.parse_args()
+    start_date = "2020-01-01"
+
+    for symbol in args.ticker:
+
+        backtest_strategy(symbol, start_date )
+        print  ("\n")
+
