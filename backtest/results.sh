256f28ca042eb69c04fcc39498f0e089f77e667f
George Vasiliu <george.vasiliu@rubylife.com>
2023-05-11 18:05:08 -0400

    backtest results update

--- backtest/results.sh
+++ backtest/results.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-rm -f results.txt; for i in `ls -1 *py | egrep -v BENCH`;do ( sleep 3; test -x $i && python3 $i | egrep Profit >> results.txt; echo >> results.txt);done
+ticker=$1
+
+rm -f results.txt; for i in `ls -1 *py | egrep -v BENCH`;do ( sleep 3; test -x $i && python3 $i $ticker | egrep Profit >> results.txt; echo >> results.txt);done
 
 date > r.tmp
-cat results.txt  | egrep AAPL | egrep -v gained | sed s'/%//g' | awk '{ print "AAPL "  $1 " " $6}' | sort -nr -k 3 >> r.tmp
+cat results.txt  | egrep -v gained | egrep '%' | sed s'/%//g' | awk '{ print $3 " "  $1 " " $6}' | sort -nr -k 3 >> r.tmp
 mv -f r.tmp results.txt
 
 git add . && git commit -m 'backtest results update' && git push
