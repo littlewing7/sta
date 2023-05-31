dd859991e65f50c41ff965b1111bc741257cdaba
George Vasiliu <george.vasiliu@rubylife.com>
2023-05-30 20:42:09 -0400

    update Tue 30 May 2023 20:42:09 EDT

--- strategies/4_CCI.py
+++ strategies/4_CCI.py
@@ -7,6 +7,59 @@ import pandas as pd
 #from pandas import DataFrame
 
 
+def backtest_strategy(stock, start_date):
+    """
+    Function to backtest a strategy
+    """
+
+    csv_file = "./data/{}_1d.csv".format( stock )
+
+    # Get today's date
+    today = datetime.datetime.now().date()
+
+    # if the file was downloaded today, read from it
+    if  ( ( os.path.exists ( csv_file ) ) and ( datetime.datetime.fromtimestamp ( os.path.getmtime ( csv_file ) ).date() == today ) ):
+        data = pd.read_csv ( csv_file, index_col='Date' )
+    else:
+        # Download data
+        data = yf.download(stock, start=start_date, progress=False)
+        data.to_csv ( csv_file )
+
+    # Calculate indicators
+    data = __CCI ( data, 20 )
+
+    # Set initial conditions
+    position = 0
+    buy_price = 0
+    sell_price = 0
+    returns = []
+
+    # Loop through data
+    for i in range(len(data)):
+
+        # Buy signal
+        if ( data['CCI_20'][i-1] < -100 ) & ( data['CCI_20'][i] > -100 ) and position == 0:
+            position = 1
+            buy_price = data["Close"][i]
+            #print(f"Buying {stock} at {buy_price}")
+
+        # Sell signal
+        elif ( data["CCI_20"][i-1] > 100 and data["CCI_20"][i] < 100 ) and position == 1:
+            position = 0
+            sell_price = data["Close"][i]
+            #print(f"Selling {stock} at {sell_price}")
+
+            # Calculate returns
+            returns.append((sell_price - buy_price) / buy_price)
+
+    # Calculate total returns
+    total_returns = (1 + sum(returns)) * 100000
+    percentage = ( ( (total_returns - 100000) / 100000) * 100)
+    percentage = "{:.0f}".format ( percentage )
+
+    return percentage + '%'
+
+
 # Optimal ticker interval for the strategy.
 timeframe = '15m'
 
@@ -17,7 +70,7 @@ data = __SMA ( data, 8 )
 data = __CCI ( data, 20 )
 
 if data['CCI_Signal'][-1] == 2:
-    print_log ( '4_CCI', 'LONG', [ 'CCI_20' ] )
+    print_log ( '4_CCI', 'LONG', [ 'CCI_20' ], backtest_strategy ( ticker , '2020-01-01' ) )
 
 if data['CCI_Signal'][-1] == -2:
-    print_log ( '4_CCI', 'SHORT', [ 'CCI_20' ] )
+    print_log ( '4_CCI', 'SHORT', [ 'CCI_20' ], backtest_strategy ( ticker , '2020-01-01' ) )
