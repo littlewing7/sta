7e2a6f248556dcf6210414227449fadd853f3550
George Vasiliu <george.vasiliu@rubylife.com>
2023-05-30 21:00:43 -0400

    update Tue 30 May 2023 21:00:43 EDT

--- strategies/119_MACD.py
+++ strategies/119_MACD.py
@@ -7,13 +7,14 @@ yesterday_data = data.iloc[-2]
 if (  yesterday_data["MACD"] <  yesterday_data["MACD_SIGNAL"] ) & ( today_data["MACD"] > today_data["MACD_SIGNAL"] ):
    print_log ( '119_MACD', 'LONG', [ 'MACD' ] )
 
-if data['MACD_Signal'][-1] == 2:
-    print_log ( '119_MACD', 'LONG', [ 'MACD CROSSOVER' ] )
-
-
 if (  yesterday_data["MACD"] >  yesterday_data["MACD_SIGNAL"] ) & ( today_data["MACD"] < today_data["MACD_SIGNAL"] ):
    print_log ( '119_MACD', 'SHORT', [ 'MACD' ] )
 
+
+
 if data['MACD_Signal'][-1] == -2:
      print_log ( '119_MACD', 'SHORT', [ 'MACD CROSSUNDER' ] )
+
+if data['MACD_Signal'][-1] == 2:
+    print_log ( '119_MACD', 'LONG', [ 'MACD CROSSOVER' ] )
 
