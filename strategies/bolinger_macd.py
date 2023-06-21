560d661ec27bcd3d2f92189f93c8f1b9f1a0b3e2
George Vasiliu <george.vasiliu@rubylife.com>
2023-06-21 19:11:50 -0400

    latest update

--- strategies/bolinger_macd.py
+++ strategies/bolinger_macd.py
@@ -3,12 +3,12 @@
 ########################
 
 
-def backtest_strategy(stock, start_date, logfile):
+def backtest_strategy(stock, start_date):
     """
     Function to backtest a strategy
     """
 
-    csv_file = "../data/{}_1d.csv".format( stock )
+    csv_file = "./data/{}_1d.csv".format( stock )
 
     # Get today's date
     today = datetime.datetime.now().date()
