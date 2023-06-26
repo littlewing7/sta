2f523c3c9a9a0ea2d88312d95e86f86ba21dba4f
George Vasiliu <george.vasiliu@rubylife.com>
2023-06-26 18:42:24 -0400

    update Mon 26 Jun 2023 18:42:24 EDT

--- indicators/tema.py
+++ indicators/tema.py
@@ -44,9 +44,6 @@ for symbol in args.ticker:
     # Calculate the TEMA
     data = __TEMA (data, 30)
 
-    # Remove the "Adj Close" column and drop any missing values
-
-
     # Check if price crosses above/below the TEMA
     close_today     = data['Adj Close'].iloc[-1]
     close_yesterday = data['Adj Close'].iloc[-2]
@@ -56,12 +53,10 @@ for symbol in args.ticker:
 
 
     if close_today > tema_30_today and close_yesterday < tema_30_yesterday:
-        print(f"TEMA30 :: STRONG BUY :: {symbol} price crossed above the TEMA on {data.index[-1].strftime('%Y-%m-%d')}")
-    elif close_today < tema_30_today and close_yesterday > tema_30_yesterday  :
-        print(f"TEMA30 :: STRONG SELL :: {symbol} price crossed below the TEMA on {data.index[-1].strftime('%Y-%m-%d')}")
-    else:
-        if close_today > tema_30_today:
-            print(f"TEMA_30 :: BUY :: {symbol} price closed above the TEMA on {data.index[-1].strftime('%Y-%m-%d')}")
-        else:
-            print(f"TEMA_30 :: SELL :: {symbol} price closed below the TEMA on {data.index[-1].strftime('%Y-%m-%d')}")
+        print(f"TEMA30 :: STRONG BUY :: {symbol} price crossed above the TEMA")
+
+    if close_today < tema_30_today and close_yesterday > tema_30_yesterday  :
+        print(f"TEMA30 :: STRONG SELL :: {symbol} price crossed below the TEMA")
+
+    print ( data.tail(3) )
 
